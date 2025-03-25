5e6ea030be240f8f5d65f7c0a9c0210ed1bcd83e
Mbaye Diagne <iagnembaye165@gmail.com>
2025-03-25 16:08:52 +0100

    feat(navbar): add navbar html structure in home.php

--- templates/home.php
+++ templates/home.php
@@ -1,13 +1,31 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="/assets/css/main.css">
     <script src="/assets/js/script.js" defer></script>
+    <!-- remixicon link css -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.6.0/remixicon.css">
 </head>
 <body>
-    <p>Bonjour homepage !</p>
+    <!-- header -->
+    <header>
+        <div class="kapoot-logo">
+            <a href="/" class="logo-kapoot-mobile"><img src="/assets/img/logo-kapoot-mobile.png" alt="site logo"></a>
+            <a href="/" class="logo-kapoot-desktop"><img src="/assets/img/logo-kapoot-desktop.png" alt="site logo"></a>
+        </div>
+        <nav class="navbar" aria-label="Main menu">
+            <ul class="navbar-group">
+                <li class="nav-link"><a href="#">Acceuil</a></li>
+                <li class="nav-link"><a href="#">Quiz</a></li>
+                <li class="nav-link"><a href="#">A propos</a></li>
+            </ul>
+        </nav>
+        <div class="nav-icon">
+            <a href="/connexion" aria-label="profil"><i class="ri-user-fill"></i></a>
+        </div>
+    </header>
 </body>
 </html>
