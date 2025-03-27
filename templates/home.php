54f9ef615cdf82dceef5fdca9d6037239c13e336
Mbaye Diagne <iagnembaye165@gmail.com>
2025-03-27 14:50:49 +0100

    feat(menu): add toggle menu html struture in home.php

--- templates/home.php
+++ templates/home.php
@@ -5,9 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kapoot</title>
     <link rel="stylesheet" href="/assets/css/main.css">
-    <script src="/assets/js/script.js" defer></script>
     <!-- remixicon link css -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.6.0/remixicon.css">
+    <!-- script js -->
+    <script src="/assets/js/script.js" defer></script>
 </head>
 <body>
     <!-- header -->
@@ -16,26 +17,40 @@
             <a href="/" class="logo-kapoot-mobile"><img src="/assets/img/logo-kapoot-mobile.png" alt="site logo"></a>
             <a href="/" class="logo-kapoot-desktop"><img src="/assets/img/logo-kapoot-desktop.png" alt="site logo"></a>
         </div>
+        <!-- navbar -->
         <nav class="navbar" aria-label="Main menu">
             <ul class="navbar-group">
-                <li class="nav-link"><a href="#">Acceuil</a></li>
-                <li class="nav-link"><a href="#">Quiz</a></li>
-                <li class="nav-link"><a href="#">A propos</a></li>
+                <li class="nav-list">
+                    <a class="nav-link" href="#">Accueil</a>
+                    <a class="nav-link" href="#">Quiz</a>
+                </li>
+                <li class="nav-icon">
+                    <i class="ri-user-fill toggleBtn"></i>
+                    <!-- toggle menu -->
+                    <div class="nav-toggler">
+                        <ul class="toggler-group">
+                            <li class="toggler-list">
+                                <a class="toggler-link" href="/connexion">Mon profil</a>
+                                <a class="toggler-link" href="/connexion">Se connecter</a>
+                                <a class="toggler-link" href="#">A propos</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
             </ul>
         </nav>
-        <div class="nav-icon">
-            <a href="/connexion" aria-label="profil"><i class="ri-user-fill"></i></a>
-        </div>
     </header>
     <!-- Main content -->
     <main role="main" class="home-page-main">
-        <p>contenu princippal</p>
+        <p>contenu principal</p>
     </main>
     <!-- Footer -->
     <footer>
         <p class="footer-text-1">&copy; KaPoot-2025</p>
         <p class="footer-text-2"><a href="#">Mention légales</a></p>
         <p class="footer-text-3"><a href="#">Politique de confidentialité</a></p>
-    </footer>
+    </footer>  
+        
+    
 </body>
 </html>
