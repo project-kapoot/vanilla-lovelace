f10cca2a0b4f4e611ea5d0c71ca44a7ff1faea8d
Mbaye Diagne <iagnembaye165@gmail.com>
2025-03-27 14:55:16 +0100

    feat(js): add toggle menu functionality

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,1 +1,8 @@
-console.log("Bonjour le monde")
+// afficher le menu déroulant
+
+const toggleBtn = document.querySelector(".toggleBtn");
+const navToggler = document.querySelector(".nav-toggler");
+
+toggleBtn.addEventListener('click', function() {
+    navToggler.classList.toggle("toggle-menu");
+});
