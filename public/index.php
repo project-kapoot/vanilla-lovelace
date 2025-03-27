f72699de8a45e9a3cfb9e4fa38954a04beabf0b1
Guillaume0402 <168349909+Guillaume0402@users.noreply.github.com>
2025-03-27 09:45:47 +0100

    feat(quiz-question): add player question page with responsive design

--- public/index.php
+++ public/index.php
@@ -6,7 +6,8 @@ $template = match($path) {
     '/' => 'home.php',
     '/connexion' => 'login.php',
     '/inscription' => 'register.php',
-    '/question' => 'question.php',
+    '/question-presentateur' => 'question-presenter.php',
+    '/question-joueur' => 'question-player.php',
     default => null
 };
 
