7295a5fd991670dcc166eab7f00e1600951d8b2a
OkaniYoshiii <dragut31620@gmail.com>
2025-03-31 10:10:39 +0200

    fix(footer): fix typo

--- templates/footer.php
+++ templates/footer.php
@@ -15,10 +15,8 @@
     <footer>
         <p class="footer-text-1">&copy; KaPoot-2025</p>
         <p class="footer-text-4"><a href="#">A propos</a></p>
-        <p class="footer-text-2"><a href="#">Mention légales</a></p>
+        <p class="footer-text-2"><a href="#">Mentions légales</a></p>
         <p class="footer-text-3"><a href="#">Politique de confidentialité</a></p>
     </footer>  
-        
-    
 </body>
 </html>
