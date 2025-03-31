5872e3a44189d522481d59ba773a0f475b723b5e
OkaniYoshiii <dragut31620@gmail.com>
2025-03-31 10:17:24 +0200

    refactor(footer): improve footer responsive

--- templates/footer.php
+++ templates/footer.php
@@ -12,11 +12,13 @@
 </head>
 <body>
     <!-- Footer -->
-    <footer>
-        <p class="footer-text-1">&copy; KaPoot-2025</p>
-        <p class="footer-text-4"><a href="#">A propos</a></p>
-        <p class="footer-text-2"><a href="#">Mentions légales</a></p>
-        <p class="footer-text-3"><a href="#">Politique de confidentialité</a></p>
+    <footer class="footer">
+        <div class="footer__inner">
+            <p class="footer-text-1">&copy; KaPoot-2025</p>
+            <p class="footer-text-4"><a href="#">A propos</a></p>
+            <p class="footer-text-2"><a href="#">Mentions légales</a></p>
+            <p class="footer-text-3"><a href="#">Politique de confidentialité</a></p>
+        </div>
     </footer>  
 </body>
 </html>
