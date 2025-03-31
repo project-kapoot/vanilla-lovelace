1b89732418af6356fcdb08485f0e06ba86e77117
OkaniYoshiii <dragut31620@gmail.com>
2025-03-31 11:12:39 +0200

    refactor: move footer/navbar into their dedicated folder

---
+++ templates/includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="footer__inner">
+        <p class="footer-text-1">&copy; KaPoot-2025</p>
+        <p class="footer-text-4"><a href="#">A propos</a></p>
+        <p class="footer-text-2"><a href="#">Mentions légales</a></p>
+        <p class="footer-text-3"><a href="#">Politique de confidentialité</a></p>
+    </div>
+</footer>  
