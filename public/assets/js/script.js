507ec3abb37a1bc2366feb2c26bf446024cbae86
OkaniYoshiii <dragut31620@gmail.com>
2025-04-01 23:20:34 +0200

    feat: add basic connection with websocket

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -18,25 +18,6 @@ document.addEventListener('click', function(ev) {
     }
 });
 
-// Remplacement du try ... catch pour une méthode plus élaborée et qui assure qu'on doit prendre en compte l'erreur
-// Voir : https://www.youtube.com/watch?v=Y6jT-IkV0VM pour une méthode qui reprend un peu ce principe
-const [dialog, error] = newDialog('dialog-profile');
-
-if(dialog !== null) {
-    dialog.openBtn.addEventListener('click', function() {
-        dialog.showModal();
-    });
-
-    dialog.closeBtn.addEventListener('click', function() {
-        dialog.close();
-    });
-
-    dialog.addEventListener('click', function(ev) {
-        if(!dialog.body.contains(ev.target)) {
-            dialog.close();
-        }  
-    });
-}
 const socket = new WebSocket('http://kapoot.localhost:443')
 
 socket.addEventListener('error', function(ev) {
