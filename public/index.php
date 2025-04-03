ab72d5d80e73cd7661cceb08c90aa5f86df97403
OkaniYoshiii <dragut31620@gmail.com>
2025-04-03 12:35:26 +0200

    refactor(routes): delete unused route and modify template/route name

--- public/index.php
+++ public/index.php
@@ -6,8 +6,7 @@ $template = match($path) {
     '/' => 'home.php',
     '/connexion' => 'login.php',
     '/inscription' => 'register.php',
-    '/question-presentateur' => 'question-presenter.php',
-    '/question-joueur' => 'question-player.php',
+    '/question' => 'question.php',
     '/profil' => 'profile.php',
     default => null
 };
