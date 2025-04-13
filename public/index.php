3d6c9c3f3b76fe40bc2fe368bba0334ea9ac7310
OkaniYoshiii <dragut31620@gmail.com>
2025-04-13 21:10:35 +0200

    fix: delete unused routes

--- public/index.php
+++ public/index.php
@@ -12,8 +12,7 @@ $template = match($path) {
     '/inscription' => 'register.php',
     '/question' => 'question.php',
     '/profil' => 'profile.php',
-    '/quiz' => 'quiz.php',
-    '/quiz-socket' => '../src/web_socket.php',
+    '/quiz/en-attente' => 'waiting_room.php',
     default => null
 };
 
