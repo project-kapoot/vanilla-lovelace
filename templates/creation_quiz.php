ad0ed511b7517f58c2e4e94f31bcd5ffdf180fa3
Mbaye Diagne <iagnembaye165@gmail.com>
2025-05-02 21:21:17 +0200

    feat(creation_quiz): add creation_quiz.php file in templates folder

---
+++ templates/creation_quiz.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Création quiz</title>
+
+    <link rel="stylesheet" href="/assets/css/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.6.0/remixicon.css">
+
+    <script src="/assets/js/script.js" type="module"></script>
+</head>
+<body>
+    <?php templatePart('navbar.php') ?>
+
+    <main>
+        <p>creation quiz page</p>
+    </main>
+
+    <?php templatePart('footer.php') ?>
+</body>
+</html>
