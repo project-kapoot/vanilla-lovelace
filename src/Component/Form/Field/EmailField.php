296802c331191b31095bb2c526f7cae563a3d840
OkaniYoshiii <dragut31620@gmail.com>
2025-05-07 20:12:55 +0200

    feat(form): add EmailField class for email validation

---
+++ src/Component/Form/Field/EmailField.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Kapoot\Form\Field;
+
+use Kapoot\Form\Validation\IsEmail;
+
+class EmailField extends TextField
+{
+    public function __construct(string $name, string $label, mixed $value)
+    {
+        parent::__construct($name, $label, $value);
+
+        $this->addValidation(new IsEmail());
+    }
+}
