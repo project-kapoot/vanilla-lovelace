61eea5666939747b5483155533bc7d769d99e0c6
Mbaye Diagne <iagnembaye165@gmail.com>
2025-05-08 20:00:09 +0200

    feat(creation-quiz): add html structure in creation_quiz.php

--- templates/creation_quiz.php
+++ templates/creation_quiz.php
@@ -11,12 +11,96 @@
     <script src="/assets/js/script.js" type="module"></script>
 </head>
 <body>
+    <!-- header -->
     <?php templatePart('navbar.php') ?>
-
-    <main>
-        <p>creation quiz page</p>
+    <!-- main content -->
+    <main class="quiz-container">
+        <div class="creation-quiz container-1200">
+            <!-- title-question -->
+            <section class="title-question">
+                <div class="thumb">
+                    <img id="" src="/assets/img/placeholder.png" alt="image du quiz">
+                    <i class="ri-add-circle-fill"></i>
+                </div>
+                <form action="">
+                    <label for="quiz-title" class="form-title">Titre du quiz :</label>
+                    <input class="form-input" type="text" name="quiz-title" id="quiz-title" placeholder="Entrer le titre du quiz" required>
+                    <label for="quiz-difficult" class="form-title">Difficulté :</label>
+                    <select name="quiz-difficult" id="quiz-difficult" required>
+                        <option value="beginner">Débutant</option>
+                        <option value="intermediate">Intermédiaire</option>
+                        <option value="advanced">Avancé</option>
+                    </select>
+                </form>
+            </section>
+            <!-- quiz-question -->
+            <section class="quiz-question">
+                <form action="">
+                    <div class="question">
+                        <label for="quiz-question" class="form-title">Question :</label>
+                        <textarea class="form-input area" name="quiz-question" id="quiz-question" placeholder="Entrer la question" required></textarea>
+                        <label for="question-type" class="form-title">Type de réponse :</label>
+                        <select name="question-type" id="question-type" required>
+                            <option value="value1">Vrai ou Faux</option>
+                            <option value="value2">Choix multiple</option>
+                            <option value="value3">Réponse numérique</option>
+                        </select>
+                    </div>
+                    <div class="boolean-response">
+                        <label class="form-title">La bonne réponse est :
+                            <p><input class="form-input" type="radio" class="reponse-true" name="question-reponse">VRAI</p>
+                            <p><input class="form-input" type="radio" class="reponse-false" name="question-reponse">FAUX</p> 
+                        </label>
+                    </div>
+                    <div class="numeric-response">
+                        <label for="numeric-response" class="form-title">Réponse numérique:</label>
+                        <input class="form-input" type="text" name="numeric-response" id="numeric-response" placeholder="Entrer la réponse" required>
+                    </div>
+                    <div class="choice-response">
+                        <label class="form-title">Réponse A :
+                            <input class="form-input" type="text" name="responseA" placeholder="Entrer la réponse" required>
+                            <p><input class="form-input" type="checkbox" class="correct-response" name="correct-response"> Est une bonne réponse</p>
+                        </label>
+                        <label class="form-title">Réponse B :
+                            <input class="form-input" type="text" name="responseB" placeholder="Entrer la réponse" required>
+                            <p><input class="form-input" type="checkbox" class="correct-response" name="correct-response"> Est une bonne réponse</p>
+                        </label>
+                        <label class="form-title">Réponse C :
+                            <input class="form-input" type="text" name="responseC" placeholder="Entrer la réponse" required>
+                            <p><input class="form-input" type="checkbox" class="correct-response" name="correct-response"> Est une bonne réponse</p>
+                        </label>
+                        <label class="form-title">Réponse D :
+                            <input class="form-input" type="text" name="responseD" placeholder="Entrer la réponse" required>
+                            <p><input class="form-input" type="checkbox" class="correct-response" name="correct-response"> Est une bonne réponse</p>
+                        </label>
+                    </div>
+                    <button id="addQuestion" type="submit" class="btn-primary">Ajouter la question</button>
+                </form>
+            </section>
+            <!-- list-qestion -->
+            <section class="list-question">
+                <h1>Cybersécurité</h1>
+                <div class="card">
+                    <div class="card-question">
+                        <h2>Intitulé de la question</h2>
+                        <div class="card-icon">
+                            <i class="ri-file-edit-line" id="edit-question"></i>
+                            <i class="ri-delete-bin-7-line" id="delete-question"></i>
+                        </div>
+                    </div>
+                    <div class="card-question">
+                        <h2>Intitulé de la question</h2>
+                        <div class="card-icon">
+                            <i class="ri-file-edit-line" id="edit-question"></i>
+                            <i class="ri-delete-bin-7-line" id="delete-question"></i>
+                        </div>
+                    </div>
+                </div>
+                <button class="btn btn-primary" id="validate-quiz">Valider le quiz</button>
+            </section>
+        </div>
     </main>
-
+    <!-- footer -->
     <?php templatePart('footer.php') ?>
 </body>
 </html>
