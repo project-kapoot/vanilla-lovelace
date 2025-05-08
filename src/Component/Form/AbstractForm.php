9f79fceb3e1add10d6c80bf203c2fc387dbf11f8
OkaniYoshiii <dragut31620@gmail.com>
2025-05-08 22:28:12 +0200

    refactor(form): delete FormValidator class for simplicity of use

--- src/Component/Form/AbstractForm.php
+++ src/Component/Form/AbstractForm.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Kapoot\Form;
 
 use Kapoot\Form\Field\AbstractField;
+use Kapoot\Form\Validation\IsRequired;
 
 abstract class AbstractForm
 {
@@ -32,5 +33,69 @@ abstract class AbstractForm
     public function getField(string $name) : AbstractField
     {
         return $this->fields->get($name);
+    }
+
+    public function validate(array $requestData) : array
+    {
+        $isSubmitted = false;
+        $isValid = false;
+        $error = null;
+
+        if(!$this->isSubmitted($requestData)) {
+            return [$isSubmitted, $isValid, $error];
+        }
+
+        $isSubmitted = true;
+
+        $formData = $requestData[$this->getName()];
+        
+        foreach($this->getFields() as $name => $field)
+        {
+            [$isValid, $error] = $this->validateField($field, $formData);
+
+            if(!$isValid && null !== $error) {
+                return [$isSubmitted, $isValid, $error];
+            }
+        }
+
+        $isValid = true;
+
+        return [$isSubmitted, $isValid, $error];
+    }
+    
+    private function validateField(AbstractField $field, mixed $formData) : array
+    {
+        $fieldData = $formData[$field->getName()] ?? null;
+
+        $fieldData = match($field->getDataType()) {
+            'integer' => (is_numeric($fieldData)) ? (int) $fieldData : $fieldData,
+            default => $fieldData
+        };
+
+        if($field->isRequired()) {
+            $validation = $field->getValidation(IsRequired::class);
+
+            if(!$validation->isValid($field, $fieldData)) {
+                return [false, $validation->getError($field, $fieldData)];
+            }
+        }
+
+        foreach($field->getValidations() as $validation)
+        {
+            if($validation->isValid($field, $fieldData)) continue;
+
+            $error = $validation->getError($field, $fieldData);
+            
+            return [false, $error];
+        }
+
+        return [true, null];
+    }
+
+    private function isSubmitted(array $requestData) : bool
+    {
+        $formData = $requestData[$this->getName()] ?? null;
+
+        return null !== $formData;
     }
 }
